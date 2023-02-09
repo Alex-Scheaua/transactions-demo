d02d8c60cff299b80c97c90d0d4da40506e5f384
Alex S <alexandru.scheaua@ext.grandcentrix.net>
2023-02-09 13:55:55 +0100

    feat(fe): Date to locale, category background

--- fe/components/transactions/TransactionTableRow.vue
+++ fe/components/transactions/TransactionTableRow.vue
@@ -1,29 +1,31 @@
 <template>
-    <tr class="bg-white border-b hover:bg-gray-50 cursor-pointer">
+    <nuxt-link :to="`transaction/${transaction.id}`" tag="tr" class="border-b hover:bg-gray-50 cursor-pointer">
         <th class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap" scope="row">
             {{ transaction.reference }}
         </th>
-        <td class="px-6 py-4">
-            {{ category }}
+        <td class="px-6 py-4 text-black" >
+            <span :style="`background-color: #${category.color}99`" class="p-2 rounded-lg">
+               {{ category.name }}
+            </span>
         </td>
         <td class="px-6 py-4">
-            {{ transaction.date }}
+            {{ new Date(transaction.date).toLocaleString() }}
         </td>
         <td class="px-6 py-4 text-right">
             {{ transaction.amount }}
             <span class="text-gray-400">
-              {{ transaction.currency }}
-            </span>
+          {{ transaction.currency }}
+        </span>
         </td>
-    </tr>
+    </nuxt-link>
 </template>
 
 <script lang="ts" setup>
-import type { Transaction } from "~/interfaces"
+import type {Category, Transaction} from "~/interfaces"
 
 defineProps<{
     transaction: Transaction
-    category: String
+    category: Category
 }>()
 </script>
 
